9e367d988464a7c3d8949facd96bfaec85c3535e
Alessandro Agozzino <alexagozzino@gmail.com>
2023-02-25 17:13:14 +0000

    recipes and ingrediente controller create remove ingredients from nesting from user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
   devise_for :users
   root to: "recipes#index"
-  get "profile", to: "pages#profile", as: :profile do
-    resources :ingredients, only: %i[new create edit update]
-  end
+  get "profile", to: "pages#profile", as: :profile
+  resources :ingredients, only: %i[index new create edit update]
   resources :recipes, only: %i[index show] do
     resources :bookmarks, only: %i[create]
     resources :reviews, only: %i[create update]
