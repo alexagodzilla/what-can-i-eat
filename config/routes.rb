979e686cf9ceb31ac806118048caae5fccd32914
Alessandro Agozzino <alexagozzino@gmail.com>
2023-02-25 15:14:22 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "recipes#index"
+  get "profile", to: "pages#profile", as: :profile do
+    resources :ingredients, only: %i[new create edit update]
+  end
+  resources :recipes, only: %i[index show] do
+    resources :bookmarks, only: %i[create]
+    resources :reviews, only: %i[create update]
+  end
+  resources :reviews, only: %i[destroy]
+  resources :bookmarks, only: %i[destroy]
+  resources :ingredients, only: %i[destroy]
 end
