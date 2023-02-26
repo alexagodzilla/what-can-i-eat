7786dc689379975cc24bf8f6db8329421abdb8bc
francesca sandford <fran6san@gmail.com>
2023-02-26 15:16:22 +0000

    add associations and validations to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :recipe_ingredients, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients
+
+  validates :title, uniqueness: true, presence: true
+  validates :instructions, :cooking_time, :serving_size, :image_url, presence: true
 end
