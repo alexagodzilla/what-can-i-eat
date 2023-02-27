96506f9d3002578291614b0821dc8093cccc608e
francesca sandford <fran6san@gmail.com>
2023-02-27 13:29:38 +0000

    update recipe schema

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,5 +5,6 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
 
   validates :title, uniqueness: true, presence: true
-  validates :instructions, :cooking_time, :serving_size, :image_url, presence: true
+  validates :instructions, :total_time, :serving_size, :image_url, presence: true
+  # validates :prep_time, :cooking_time, presence: true
 end
