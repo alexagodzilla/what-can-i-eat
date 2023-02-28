92a86f9caf280c842cc709950f4785eb33357a77
francesca sandford <fran6san@gmail.com>
2023-02-28 09:24:34 +0000

    create recipe show page and helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def truncate_zero(ingredient_quantity)
+    if ingredient_quantity.to_s.end_with?('.0')
+      ingredient_quantity.to_i
+    else
+      ingredient_quantity.round(2)
+    end
+  end
 end
