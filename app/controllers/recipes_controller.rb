d08c12e902d4483f87fb28472dc2b215d1681217
Alessandro Agozzino <alexagozzino@gmail.com>
2023-02-28 19:30:38 +0000

    validation add to bookmark and redirect to the right places

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,5 +5,7 @@ class RecipesController < ApplicationController
   def show
     @bookmark = Bookmark.new
     @recipe = Recipe.find(params[:id])
+    @recipe_bookmark = Bookmark.where(recipe_id: @recipe)
+    @current_user_bookmarks = Bookmark.where(user: current_user)
   end
 end
