acb9259f1750e6b5398ab16d30aa151286d2079f
francesca sandford <fran6san@gmail.com>
2023-02-28 09:59:20 +0000

    remove code that we won't use right now

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,13 +5,4 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
-
-  private
-
-  def recipe_params
-    params.require(:recipe).permit(:title, :instructions, :total_time, :serving_size, :image_url,
-                                   :vegetarian, :vegan, :dairy_free, :gluten_free)
-  end
 end
-
-# pls note that i've not included prep_time and cooking_time in params for now
