77309eb7df3c6eca81702e79720e5ae9836b8ee8
francesca sandford <fran6san@gmail.com>
2023-02-28 18:24:46 +0000

    implement reviews create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,26 @@
 class ReviewsController < ApplicationController
   def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.recipe = @recipe
+    if @review.save!
+      redirect_to recipe_path(@recipe), notice: "Comment successfully added"
+    else
+      render "recipes/show", status: :unprocessable_entity
+    end
+    # going to update the redirecting
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
 
-  validates :content, presence: true
   validates :rating, inclusion: { in: 0..5 }
 end
