e10e76891e8db1cf098aa3ad102e76fc323fdbf6
Francesca Sandford <114738789+fransan6@users.noreply.github.com>
2023-03-02 19:47:56 +0000

    Update recipes_controller.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,25 +2,10 @@ class RecipesController < ApplicationController
   def index
     if params[:query].present?
       @recipes = Recipe.search_by_title_and_instructions(params[:query])
-      # @recipes = @recipes.search(params[:query]) if params[:query].present?
-      # @recipes = @recipes.vegetarian if params[:vegetarian].present?
-      # @recipes = @recipes.vegan if params[:vegan].present?
-      # @recipes = @recipes.gluten_free if params[:gluten_free].present?
-      # @recipes = @recipes.dairy_free if params[:dairy_free].present?
-      # @recipes = @recipes.with_total_time(params[:total_time]) if params[:total_time].present?
       # @recipes = @recipes.with_ingredients(params[:ingredients].split(',')) if params[:ingredients].present?
-      # @recipes = @recipes.sorted_by(params[:sort_by]) if params[:sort_by].present?
     else
       @recipes = Recipe.all
-      # @recipes = @recipes.search(params[:query]) if params[:query].present?
-      # @recipes = @recipes.vegetarian if params[:vegetarian].present?
-      # @recipes = @recipes.vegan if params[:vegan].present?
-      # @recipes = @recipes.gluten_free if params[:gluten_free].present?
-      # @recipes = @recipes.dairy_free if params[:dairy_free].present?
-      # @recipes = @recipes.with_total_time(params[:total_time]) if params[:total_time].present?
-      # @recipes = @recipes.with_ingredients(params[:ingredients].split(',')) if params[:ingredients].present?
-      # @recipes = @recipes.sorted_by(params[:sort_by]) if params[:sort_by].present?
-  end
+    end
   end
 
   def show
