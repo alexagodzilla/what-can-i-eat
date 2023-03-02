d7bf46865503b8da7dcae82b3e8fca3494db7def
Jon Dedman <jonathandedman@googlemail.com>
2023-03-02 12:47:09 +0000

    update model for full-text search using pgsearch

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+include PgSearch::Model
+
   has_many :reviews, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_many :recipe_ingredients, dependent: :destroy
@@ -7,4 +9,10 @@ class Recipe < ApplicationRecord
   validates :title, uniqueness: true, presence: true
   validates :instructions, :total_time, :serving_size, :image_url, presence: true
   # validates :prep_time, :cooking_time, presence: true
+
+  pg_search_scope :search_by_ingredient_names,
+    associated_against: {
+      ingredients: :name
+    }
+
 end
