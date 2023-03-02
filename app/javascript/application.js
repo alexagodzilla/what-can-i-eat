bd84bedf3ae7acf430445db737c40bbb2398ab14
Jon Dedman <jonathandedman@googlemail.com>
2023-03-02 15:03:41 +0000

    implement multi-ingreedient search using stimulus and pgsearch

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,11 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "bootstrap"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "bootstrap";
+
+import { application } from "../controllers/index.js";
+import SearchFormController from "../controllers/search_form_controller.js";
+import SearchResultsController from "../controllers/search_results_controller.js";
+
+application.register("search-form", SearchFormController);
+application.register("search-results", SearchResultsController);
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,4 @@
 class Recipe < ApplicationRecord
-include PgSearch::Model
 
   has_many :reviews, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
@@ -8,11 +7,43 @@ include PgSearch::Model
 
   validates :title, uniqueness: true, presence: true
   validates :instructions, :total_time, :serving_size, :image_url, presence: true
-  # validates :prep_time, :cooking_time, presence: true
+  validates :prep_time, :cooking_time, presence: true
 
-  pg_search_scope :search_by_ingredient_names,
-    associated_against: {
-      ingredients: :name
-    }
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_instructions,
+                  against: [:title, :instructions],
+                  using: {
+                    tsearch: { prefix: true }}
+  # scope :vegetarian, -> { where(vegetarian: true) }
+  # scope :vegan, -> { where(vegan: true) }
+  # scope :gluten_free, -> { where(gluten_free: true) }
+  # scope :dairy_free, -> { where(dairy_free: true) }
 
+  # scope :with_total_time, ->(total_time) {
+  #   where('total_time <= ?', total_time.to_i) unless total_time.blank?
+  # }
+
+  # scope :with_ingredients, ->(ingredients) {
+  #   joins(:ingredients).where("ingredients.name IN (?)", ingredients) unless ingredients.blank?
+  # }
+
+  # def self.search(query)
+  #   if query.present?
+  #     search_by_title_and_instructions(query)
+  #   else
+  #     all
+  #   end
+  # end
+
+  # def self.sorted_by(sort_option)
+  #   direction = /desc$/.match?(sort_option) ? "desc" : "asc"
+  #   case sort_option.to_s
+  #   when /^title_/
+  #     order("title #{direction}")
+  #   when /^created_at_/
+  #     order("created_at #{direction}")
+  #   else
+  #     raise ArgumentError, "Invalid sort option: #{sort_option.inspect}"
+  #   end
+  # end
 end
