ead90633b51b9ca6b2b71c5f85f6e04683fb944f
francesca sandford <fran6san@gmail.com>
2023-03-03 17:34:34 +0000

    fix errors

--- app/javascript/controllers/search_results_controller.js
+++ app/javascript/controllers/search_results_controller.js
@@ -1,9 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="search-results"
-// search_results_controller.js
-import { Controller } from "stimulus";
-
 export default class extends Controller {
 	static targets = ["list"];
 
