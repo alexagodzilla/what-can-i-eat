3b37de520a1bec62ebf7bf108d57e001bcc6c1ee
Jon Dedman <jonathandedman@googlemail.com>
2023-03-03 10:51:27 +0000

    add comments to the stimulus code

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -4,26 +4,37 @@ import { Controller } from "@hotwired/stimulus";
 import { Controller } from "stimulus";
 
 export default class extends Controller {
+	// The form element has a data-target attribute with the name of the input
 	static targets = ["query", "ingredients"];
-
+	// The form element has a data-action attribute with the name of the method which is called search()
 	search() {
+		// Get the values from the form
 		const query = this.queryTarget.value;
+		// Split the ingredients string into an array
 		const ingredients = this.ingredientsTarget.value
 			.split(",")
 			.map((s) => s.trim());
+		// Create a new URL object from the form action
 		const url = new URL(this.element.action);
-
+		// Set the query and ingredients parameters on the URL object
 		url.searchParams.set("query", query);
 		url.searchParams.set("ingredients[]", ingredients);
-
+		// Fetch the URL which contains the query and ingredients parameters
 		fetch(url)
+			// Get the response as text
 			.then((response) => response.text())
+			// parse the response as HTML
 			.then((html) => {
+				// this.element is the form element
 				this.element
+					// Find the closest parent element with a data-controller attribute with the value of turbo-stream
 					.closest("[data-controller='turbo-stream']")
+					// Dispatch a custom event with the name turbo:stream:update and the detail of the HTML
 					.dispatchEvent(
+						// Create a new CustomEvent with the name turbo:stream:update and the detail of the HTML which is the response from the fetch
 						new CustomEvent("turbo:stream:update", { detail: html })
 					);
 			});
 	}
 }
+// the dispatchEvent() method dispatches an event at the specified target, which is the closest parent element with a data-controller attribute with the value of turbo-stream. In this case, the form element is the target. This information is passed to the event handler in the search_results_controller.js file.
--- app/javascript/controllers/search_results_controller.js
+++ app/javascript/controllers/search_results_controller.js
@@ -1,14 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
-
+// html from the seach_form controller is passed to the search_results controller
 // Connects to data-controller="search-results"
 // search_results_controller.js
 import { Controller } from "stimulus";
 
 export default class extends Controller {
 	static targets = ["list"];
-
+	// The event handler for the turbo:stream:update event. The event handler is called when the form element dispatches the turbo:stream:update event. The event handler receives the event object as an argument. The event handler sets the innerHTML of the listTarget to the HTML from the fetch request. The listTarget is the element with a data-target attribute with the value of list. update() is a method that is called when the turbo:stream:update event is dispatched. It receives the event object as an argument. The event object has a detail property which contains the HTML from the fetch request in the search_form_controller.js file. The event handler sets the innerHTML of the listTarget to the HTML from the fetch request. The listTarget is the element with a data-target attribute with the value of list.
 	update(event) {
-		const [data, status, xhr] = event.detail;
+		// data is the HTML from the fetch request. it contains the HTML from the search_results view. it is destructured from the event.detail array. destructuring is a JavaScript feature that allows you to extract data from an array or object and assign it to a variable. The event.detail array contains the HTML from the fetch request. the HTML is assigned to the data variable. The event.detail array contains the HTML from the fetch request. The HTML is assigned to the data variable.
+		const [data] = event.detail;
 		this.listTarget.innerHTML = data;
 	}
 }
+
+// The syntax for desstructuring depends on the type of data that is being destructured. If the data is an array, the syntax is [data] = event.detail. If the data is an object, the syntax is { data } = event.detail.
