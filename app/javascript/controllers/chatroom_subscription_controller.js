c1cea5222afed1874016338fe2101481624ba9df
Alessandro Agozzino <alexagozzino@gmail.com>
2023-03-04 11:32:11 +0000

    fix stimulus chat_subscription_controller

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,10 +6,10 @@ export default class extends Controller {
   static targets = ["messages"]
 
   connect() {
-    this.channel = createConsumer().subscriptions.create(
-      { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.#insertMessageAndScrollDown(data) }
-    )
+    // this.channel = createConsumer().subscriptions.create(
+    //   { channel: "ChatroomChannel", id: this.chatroomIdValue },
+    //   { received: data => this.#insertMessageAndScrollDown(data) }
+    // )
     console.log("Hello from stimulus, you are currently getting messagesTarget:", this.messagesTarget)
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
