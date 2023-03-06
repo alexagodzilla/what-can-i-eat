0ace4e24a1a1170ebe00d284d92f3d01b7d64c2c
francesca sandford <fran6san@gmail.com>
2023-03-06 19:39:04 +0000

    fix bug and reformat some of seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
   has_many :user_ingredients, dependent: :destroy
   has_many :ingredients, through: :user_ingredients
   has_many :messages
+  has_many :chatrooms, through: :messages
+  # is this new through: association correct?
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 2 }
+  validates :first_name, presence: true
 end
