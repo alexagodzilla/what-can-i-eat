ff6cd04f6ef98765deb651f17dfc16d5b9e4a75f
Francesca Sandford <114738789+fransan6@users.noreply.github.com>
2023-03-06 20:14:32 +0000

    Update user.rb
    remove unnecessary association

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,6 @@ class User < ApplicationRecord
   has_many :user_ingredients, dependent: :destroy
   has_many :ingredients, through: :user_ingredients
   has_many :messages
-  has_many :chatrooms, through: :messages
-  # is this new through: association correct?
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 2 }
   validates :first_name, presence: true
