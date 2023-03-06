2cef45eb7009c1ba99b3ea1386e08fe9361d3f94
Alessandro Agozzino <alexagozzino@gmail.com>
2023-03-06 12:53:28 +0000

    This commit implementsajax functionalities to the chat

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,11 +6,28 @@ export default class extends Controller {
   static targets = ["messages"]
 
   connect() {
-    // this.channel = createConsumer().subscriptions.create(
-    //   { channel: "ChatroomChannel", id: this.chatroomIdValue },
-    //   { received: data => this.#insertMessageAndScrollDown(data) }
-    // )
+
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.#insertMessageAndScrollDown(data) }
+    )
     console.log("Hello from stimulus, you are currently getting messagesTarget:", this.messagesTarget)
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
   }
+
+  disconnect(){
+    console.log("disconnected")
+    this.channel.unsubscribe()
+  }
+
+  resetForm(event) {
+    const form = event.target
+    form.reset()
+  }
+
+  #insertMessageAndScrollDown(data){
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
 }
