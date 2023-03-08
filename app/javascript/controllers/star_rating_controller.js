135f4c944423b0ebcaeefdfbe85b19599aeda175
Francesca Sandford <114738789+fransan6@users.noreply.github.com>
2023-03-08 20:33:58 +0000

    Update star_rating_controller.js

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -1,9 +1,9 @@
-import { Controller } from "@hotwired/stimulus"
-import StarRating from "star-rating.js"
+// import { Controller } from "@hotwired/stimulus"
+// import StarRating from "star-rating.js"
 
 // Connects to data-controller="star-rating"
-export default class extends Controller {
-  connect() {
-    new StarRating(this.element)
-  }
-}
+// export default class extends Controller {
+//  connect() {
+//    new StarRating(this.element)
+//  }
+// }
