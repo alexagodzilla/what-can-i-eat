cc0b15aa84a820cd4ddc5110387a1166150fd849
francesca sandford <fran6san@gmail.com>
2023-03-09 19:59:51 +0000

    draft of profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,14 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
 
-  def home
-  end
-
   def profile
     @chatroom = Chatroom.first
     @user_bookmarks = Bookmark.where(user: current_user)
     @user_ingredients = UserIngredient.where(user: current_user)
     @user_ingredient = UserIngredient.new
+  end
+
+  def friends
+    @friends = User.take(6)
   end
 end
