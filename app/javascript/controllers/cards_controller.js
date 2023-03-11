0c29a781f05a02b937da2617b3b99f017dec5719
Alessandro Agozzino <alexagozzino@gmail.com>
2023-03-11 17:05:25 +0000

    Rating filter

--- app/javascript/controllers/cards_controller.js
+++ app/javascript/controllers/cards_controller.js
@@ -2,31 +2,17 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="cards"
 export default class extends Controller {
-  static targets = ["card", "collection"]
-  connect() {
-
-  }
+  static targets = ["card", "container"]
 
   sortCardsAsc() {
-    this.cardTargets.sort((a, b) => {
-      if (a.dataset.averageRating < b.dataset.averageRating) return -1;
-      if (a.dataset.averageRating > b.dataset.averageRating) return 1;
-      return 0;
-    });
-    this.collectionTarget.innerHTML = '';
-    this.cardTargets.forEach((card) => console.log());
-    // this.collectionTarget.innerHTML = '';
-    // this.cardTargets.forEach((card) => {
-    //   this.collectionTarget.insertAdjacentHTML('beforeend', card);
-    // });
+    this.cardTargets
+      .sort((a, b) => parseFloat(a.dataset.averageRating) - parseFloat(b.dataset.averageRating))
+      .forEach((card) => this.containerTarget.appendChild(card));
   };
 
   sortCardsDesc() {
-    this.cardTargets.sort((a, b) => {
-      if (a.dataset.averageRating < b.dataset.averageRating) return 1;
-      if (a.dataset.averageRating > b.dataset.averageRating) return -1;
-      return 0;
-    });
+    this.cardTargets
+      .sort((a, b) => parseFloat(b.dataset.averageRating) - parseFloat(a.dataset.averageRating))
+      .forEach((card) => this.containerTarget.appendChild(card));
   };
-
 }
