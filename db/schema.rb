65c9b07b814046bbcc8b830eb2a168725d2fc77b
Jon Dedman <jonathandedman@googlemail.com>
2023-03-12 22:27:20 +0000

    fix footer on pages with less content

--- db/schema.rb
+++ db/schema.rb
@@ -7,6 +7,7 @@
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
+#
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_11_170206) do
