f78ec2bc89815aa29f033ab80ab4d6ab2308b343
francesca sandford <fran6san@gmail.com>
2023-03-12 17:57:17 +0000

    implement functionality to add a friend through reviews

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,11 @@
 class FriendshipsController < ApplicationController
+  def create
+    @friendship = Friendship.new
+    @friendship.requester = current_user
+    @friendship.requested = User.find(params[:friendship][:requested])
+    @friendship.status = "pending"
+    return unless @friendship.save!
+
+    redirect_back_or_to profile_friends_path, notice: "Invite sent to #{@friendship.requested.username}"
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,5 @@
 module ApplicationHelper
   def truncate_zero(ingredient_quantity)
-    if ingredient_quantity.to_s.end_with?('.0')
-      ingredient_quantity.to_i
-    else
-      ingredient_quantity.round(2)
-    end
-  end
-
-  def sort_recipes_by_rating(recipes, low_to_high = false)
-    res = recipes.sort_by(&:average_rating)
-    low_to_high ? res : res.reverse
+    ingredient_quantity.to_s.end_with?('.0') ? ingredient_quantity.to_i : ingredient_quantity.round(2)
   end
 end
