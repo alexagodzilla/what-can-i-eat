3f04510e17684c26e8315209fe9d14c7631851e3
Francesca Sandford <114738789+fransan6@users.noreply.github.com>
2023-03-14 18:49:02 +0000

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,10 +11,6 @@ class PagesController < ApplicationController
   def friends
     @already_friends = current_user.friends
     @pending_friends = current_user.pending_friends
-    # @friendships = []
-    # @pending_friends.each do |person|
-    #   @friendships << Friendship.where(requested: current_user, requester: person, status: "pending")
-    # end
     @friendships = friendships_finder(@pending_friends)
     @friendship_instance = Friendship.where(requested: current_user)
   end
@@ -24,5 +20,4 @@ class PagesController < ApplicationController
       Friendship.where(requested: current_user, requester: person)
     end
   end
-
 end
