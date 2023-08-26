60ab914aeca9387f6eea2c4b52ec6a0db23a047b
francesca sandford <fran6san@gmail.com>
2023-08-26 10:35:39 +0100

    refactor: add methods to seed files

--- db/seeds/01_recipes.rb
+++ db/seeds/01_recipes.rb
@@ -1,71 +1,88 @@
 include ActionView::Helpers::SanitizeHelper
 require "json"
 
-puts "reading json files"
-["vegetarian_recipes", "recipe_api_data"].each do |file_name|
-  root_path = "#{Rails.root}/public/recipes_data/#{file_name}.json"
-  recipes = JSON.parse(File.read(root_path), symbolize_names: true)[:recipes]
+# methods used in create_recipes
+def create_recipe_instructions(recipe, instructions)
+  if instructions.nil?
+    recipe[:summary]
+  else
+    strip_tags(recipe[:instructions]).gsub(/(\w+)\.(\w+)/, '\1. \2')
+  end
+end
 
-  puts "creating recipes from #{file_name}"
+def create_recipe(recipe, instructions)
+  Recipe.create!(
+    title: recipe[:title],
+    instructions:,
+    total_time: recipe[:readyInMinutes],
+    serving_size: recipe[:servings],
+    image_url: recipe[:image] || "https://unsplash.com/photos/ZrhtQyGFG6s",
+    vegetarian: recipe[:vegetarian],
+    vegan: recipe[:vegan],
+    dairy_free: recipe[:dairyFree],
+    gluten_free: recipe[:glutenFree],
+    average_rating: 0.0
+  )
+end
+
+def create_steps(recipe, new_db_recipe)
+  recipe[:analyzedInstructions][0][:steps].each do |step|
+    Step.create!(
+      number: step[:number],
+      content: step[:step],
+      recipe_id: new_db_recipe.id
+    )
+  end
+end
+
+def create_ingredient(recipe, new_db_recipe)
+  recipe[:extendedIngredients].each do |api_ingredient|
+    db_ingredient = Ingredient.find_by(name: api_ingredient[:name].capitalize.gsub(/\d+/, '').strip)
+    next unless db_ingredient.nil?
+
+    ingredient(api_ingredient)
+    next if new_db_recipe.ingredients.include?(db_ingredient) && db_ingredient.recipes.include?(new_db_recipe)
+
+    recipe_ingredient(api_ingredient, new_db_recipe, db_ingredient)
+  end
+end
+
+def ingredient(api_ingredient)
+  if api_ingredient[:unit].include?("cup")
+    unit = api_ingredient[:unit].downcase
+  else
+    unit = api_ingredient[:measures][:metric][:unitShort].downcase
+  end
+  Ingredient.create!(
+    name: api_ingredient[:name].capitalize.gsub(/\d+/, '').strip,
+    quantity_unit: unit
+  )
+end
+
+def recipe_ingredient(api_ingredient, new_db_recipe, db_ingredient)
+  RecipeIngredient.create!(
+    quantity: api_ingredient[:amount],
+    recipe_id: new_db_recipe.id,
+    ingredient_id: db_ingredient.id
+  )
+end
+
+# the seeding
+def create_recipes(recipes)
   recipes.each do |recipe|
     next unless Recipe.find_by(title: recipe[:title]).nil?
 
-    if recipe[:image].nil?
-      img = "https://unsplash.com/photos/ZrhtQyGFG6s"
-    else
-      img = recipe[:image]
-    end
-
-    if recipe[:instructions].nil?
-      instructions = recipe[:summary]
-    else
-      instructions = strip_tags(recipe[:instructions]).gsub(/(\w+)\.(\w+)/, '\1. \2')
-    end
-
-    new_db_recipe = Recipe.create!(
-      title: recipe[:title],
-      instructions:,
-      total_time: recipe[:readyInMinutes],
-      serving_size: recipe[:servings],
-      image_url: img,
-      vegetarian: recipe[:vegetarian],
-      vegan: recipe[:vegan],
-      dairy_free: recipe[:dairyFree],
-      gluten_free: recipe[:glutenFree],
-      average_rating: 0.0
-    )
-
-    unless recipe[:analyzedInstructions].empty?
-      recipe[:analyzedInstructions][0][:steps].each do |step|
-        Step.create!(
-          number: step[:number],
-          content: step[:step],
-          recipe_id: new_db_recipe.id
-        )
-      end
-    end
-
-    recipe[:extendedIngredients].each do |api_ingredient|
-      db_ingredient = Ingredient.find_by(name: api_ingredient[:name].capitalize.gsub(/\d+/, '').strip)
-      if db_ingredient.nil?
-        if api_ingredient[:unit].include?("cup")
-          unit = api_ingredient[:unit].downcase
-        else
-          unit = api_ingredient[:measures][:metric][:unitShort].downcase
-        end
-        db_ingredient = Ingredient.create!(
-          name: api_ingredient[:name].capitalize.gsub(/\d+/, '').strip,
-          quantity_unit: unit
-        )
-      end
-
-      unless new_db_recipe.ingredients.include?(db_ingredient) && db_ingredient.recipes.include?(new_db_recipe)
-        RecipeIngredient.create!(
-          quantity: api_ingredient[:amount],
-          recipe_id: new_db_recipe.id,
-          ingredient_id: db_ingredient.id
-        )
-      end
-    end
+    instructions = create_recipe_instructions(recipe, recipe[:instructions])
+    new_db_recipe = create_recipe(recipe, instructions)
+    create_steps(recipe, new_db_recipe) unless recipe[:analyzedInstructions].empty?
+    create_ingredient(recipe, new_db_recipe)
   end
+end
+
+["vegetarian_recipes", "recipe_api_data"].each do |file_name|
+  puts "Reading JSON file"
+  root_path = "#{Rails.root}/public/recipes_data/#{file_name}.json"
+  recipes = JSON.parse(File.read(root_path), symbolize_names: true)[:recipes]
+  puts "Creating recipes from #{file_name}"
+  create_recipes(recipes)
 end
--- db/seeds/02_users.rb
+++ db/seeds/02_users.rb
@@ -1,100 +1,68 @@
 require "faker"
 
-twenty_avatar_imgs = [
+avatar_imgs = [
+  "https://randomuser.me/api/portraits/women/44.jpg",
+  "https://randomuser.me/api/portraits/women/63.jpg",
+  "https://randomuser.me/api/portraits/women/47.jpg",
+  "https://randomuser.me/api/portraits/women/95.jpg",
+  "https://randomuser.me/api/portraits/women/79.jpg",
+  "https://randomuser.me/api/portraits/women/14.jpg",
+  "https://randomuser.me/api/portraits/women/8.jpg",
+  "https://randomuser.me/api/portraits/women/9.jpg",
+  "https://randomuser.me/api/portraits/women/33.jpg",
+  "https://randomuser.me/api/portraits/women/48.jpg",
   "https://randomuser.me/api/portraits/men/32.jpg",
   "https://randomuser.me/api/portraits/men/29.jpg",
   "https://randomuser.me/api/portraits/men/35.jpg",
   "https://randomuser.me/api/portraits/men/4.jpg",
+  "https://randomuser.me/api/portraits/men/7.jpg",
   "https://randomuser.me/api/portraits/men/22.jpg",
   "https://randomuser.me/api/portraits/men/12.jpg",
   "https://randomuser.me/api/portraits/men/52.jpg",
-  "https://images.unsplash.com/photo-1455354269813-737d9df115bb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=1229aa0db2a9a42022b7669f30784123",
-  "https://images.unsplash.com/photo-1502452213786-a5bc0a67e963?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=8474b116817dfa40dc19c9349a16ee5a",
-  "https://images.pexels.com/photos/450212/pexels-photo-450212.jpeg?h=350&auto=compress&cs=tinysrgb",
   "https://randomuser.me/api/portraits/men/84.jpg",
-  "https://randomuser.me/api/portraits/men/16.jpg",
-  "https://images.unsplash.com/photo-1474533410427-a23da4fd49d0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=ee9537f6365657688885825712e3349d",
-  "https://images.unsplash.com/photo-1503593245033-a040be3f3c82?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=ca8c652b62b1f14c9c4c969289a8b33c",
-  "https://randomuser.me/api/portraits/women/44.jpg",
-  "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9",
-  "https://randomuser.me/api/portraits/women/63.jpg",
-  "https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=3759e09a5b9fbe53088b23c615b6312e",
-  "https://randomuser.me/api/portraits/women/47.jpg",
-  "https://randomuser.me/api/portraits/women/95.jpg",
-  "https://randomuser.me/api/portraits/women/79.jpg",
-  "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=046c29138c1335ef8edee7daf521ba50",
-  "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-  "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-  "https://randomuser.me/api/portraits/women/8.jpg",
-  "https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-  "https://randomuser.me/api/portraits/women/9.jpg",
-  "https://images.unsplash.com/photo-1496081081095-d32308dd6206?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=dd302358c7e18c27c4086e97caf85781",
-  "https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb",
-  "https://images.unsplash.com/photo-1546539782-6fc531453083?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
-  "https://api.uifaces.co/our-content/donated/AVQ0V28X.jpg",
-  "https://randomuser.me/api/portraits/women/33.jpg",
-  "https://images.pexels.com/photos/325531/pexels-photo-325531.jpeg?h=350&auto=compress&cs=tinysrgb",
-  "https://images.unsplash.com/photo-1465406325903-9d93ee82f613?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=5eb9f23d01faf52817ff797530242521"
+  "https://randomuser.me/api/portraits/men/16.jpg"
+]
+specific_users = [
+  {
+    first_name: "Ila",
+    last_name: "Peroni",
+    image_url: "https://avatars.githubusercontent.com/u/114817089?v=4"
+  },
+  {
+    first_name: "Alex",
+    last_name: "Agozzino",
+    diet: "Gluten-free",
+    image_url: "https://avatars.githubusercontent.com/u/59085737?v=4"
+  },
+  {
+    first_name: "Jon",
+    last_name: "Dedman",
+    image_url: "https://avatars.githubusercontent.com/u/110668469?v=4"
+  },
+  {
+    first_name: "Fran",
+    last_name: "Sandford"
+  }
 ]
 
-puts "creating users"
-20.times do
+def create_users(user, avatar_imgs = [])
   User.create!(
-    first_name: Faker::Name.unique.first_name,
-    last_name: Faker::Name.unique.last_name,
+    first_name: user[:first_name] || Faker::Name.unique.first_name,
+    last_name: user[:last_name] || Faker::Name.unique.last_name,
     username: Faker::Internet.unique.username,
-    email: Faker::Internet.unique.email,
+    email: user[:first_name] ? "#{user[:first_name].downcase}@me.com" : Faker::Internet.unique.email,
     password: "123456",
-    bio: Faker::Hipster.paragraph(sentence_count: 2),
-    diet: ["Vegetarian", "Vegan", "Gluten Free", "Dairy Free", "Everything"].sample,
-    image_url: twenty_avatar_imgs.sample
+    bio: Faker::Hipster.paragraph(sentence_count: 3),
+    diet: user[:diet] || ["Vegetarian", "Vegan", "Gluten-free", "Dairy-free", "Everything"].sample,
+    image_url: user[:image_url] || avatar_imgs.sample
   )
 end
 
-puts "creating Alex (alex@me.com)"
-ALEX = User.create!(
-  first_name: "Alex",
-  last_name: "Agozzino",
-  username: Faker::Internet.unique.username,
-  email: "alex@me.com",
-  password: "123456",
-  bio: Faker::Hipster.paragraph(sentence_count: 2),
-  diet: "Gluten Free",
-  image_url: "https://avatars.githubusercontent.com/u/59085737?v=4"
-)
+# creates 20 random users
+20.times { create_users({}, avatar_imgs) }
 
-puts "creating Fran (fran@me.com)"
-FRAN = User.create!(
-  first_name: "Fran",
-  last_name: "Sandford",
-  username: Faker::Internet.unique.username,
-  email: "fran@me.com",
-  password: "123456",
-  bio: Faker::Hipster.paragraph(sentence_count: 2),
-  diet: "Vegetarian",
-  image_url: "https://avatars.githubusercontent.com/u/114738789?v=4"
-)
-
-puts "creating Ila (ila@me.com)"
-ILA = User.create!(
-  first_name: "Ila",
-  last_name: "Peroni",
-  username: Faker::Internet.unique.username,
-  email: "ila@me.com",
-  password: "123456",
-  bio: Faker::Hipster.paragraph(sentence_count: 2),
-  diet: "Gluten Free",
-  image_url: "https://avatars.githubusercontent.com/u/114817089?v=4"
-)
-
-puts "creating Jon (jon@me.com)"
-JON = User.create!(
-  first_name: "Jon",
-  last_name: "Dedman",
-  username: Faker::Internet.unique.username,
-  email: "jon@me.com",
-  password: "123456",
-  bio: Faker::Hipster.paragraph(sentence_count: 2),
-  diet: "Everything",
-  image_url: "https://avatars.githubusercontent.com/u/110668469?v=4"
-)
+# creates 4 specific users
+ILA = create_users(specific_users[0])
+ALEX = create_users(specific_users[1])
+JON = create_users(specific_users[2])
+FRAN = create_users(specific_users[3], avatar_imgs)
